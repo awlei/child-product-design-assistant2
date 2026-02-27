8e9b1fe4bbd3fbd2f43f86b9151c3bfd728f71a2
user1329266802 <2526015764586800-user1329266802@noreply.coze.cn>
2026-02-27 14:32:47 +0800

    fix: 在 gradle.properties 中强制指定 Compose Compiler 版本为 1.5.4

--- android-project/app/build.gradle.kts
+++ android-project/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp") version "1.9.20-1.0.14"
 }
 
@@ -64,6 +63,10 @@ android {
 
     buildFeatures {
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
 
     packaging {
--- android-project/build.gradle.kts
+++ android-project/build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
-    // Compose Compiler Plugin (推荐方式)
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
