86152b22c62894673f142e48219e9fdb77578d6a
user1329266802 <2526015764586800-user1329266802@noreply.coze.cn>
2026-02-27 11:33:38 +0800

    fix: 升级 Kotlin 到 1.9.20 和 Compose Compiler 到 1.5.14 以匹配版本兼容性

--- android-project/build.gradle.kts
+++ android-project/build.gradle.kts
@@ -1,35 +1,8 @@
 // Top-level build file
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.10-1.0.15")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.15" apply false
-}
-
-// Force Kotlin version across the entire project
-allprojects {
-    configurations.all {
-        resolutionStrategy {
-            force("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
-            force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.10")
-            force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10")
-            force("org.jetbrains.kotlin:kotlin-stdlib-common:1.9.10")
-            force("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
-            force("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.9.10")
-            force("org.jetbrains.kotlin:kotlin-script-runtime:1.9.10")
-        }
-    }
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
 
 tasks.register("clean", Delete::class) {
